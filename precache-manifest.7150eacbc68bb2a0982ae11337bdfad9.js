2f0a4a6e2c44a096155c470976be8b39b790e640
Misha Lysak <myhaillysak@gmail.com>
2021-03-11 20:24:28 +0200

    Updates

---
+++ precache-manifest.7150eacbc68bb2a0982ae11337bdfad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be5d1d88b35b5f7f373d311fd537b4e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "60a139b9327c03bea6d9",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "98f13019859d35b3b59b",
+    "url": "./static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "60a139b9327c03bea6d9",
+    "url": "./static/js/2.d10b847f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d10b847f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f13019859d35b3b59b",
+    "url": "./static/js/main.ae838110.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
